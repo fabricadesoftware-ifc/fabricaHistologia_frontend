730932fc3158bb59ff7fc9f3333f08ae3df44075
Gabz047 <gabe1612mail@gmail.com>
2024-08-07 09:28:36 -0300

    Fix in Initial Page responsivity and added icons

--- src/components/InitialPageComp.vue
+++ src/components/InitialPageComp.vue
@@ -6,17 +6,17 @@ import BoxButtonComp from '@/components/BoxButtonComp.vue'
 </script>
 <template>
     <main class=" grid grid-cols-5 grid-rows-3 w-dvw h-dvh bg-cover bg-top md:flex main-Initial-Page">
-        <div class="row-start-1 col-start-3 col-end-6 md:hidden flex items-start justify-end absolute w-full z-10">
+        <div class="row-start-1 col-start-3 col-end-6 xl:hidden flex items-start justify-end absolute w-full z-10">
         <div class="h-20 w-12/12 flex items-center mt-5 mr-8">
         <BoxButtonComp />
         </div>
         </div>
-        <div class="col-start-2 col-end-5 row-span-3 md:justify-between flex flex-col items-center justify-center">
+        <div class="col-start-1 col-end-6 w-full h-full row-span-3 md:justify-between flex flex-col items-center justify-center absolute">
             <TitleInitialPageComp  class="z-10" />
-            <InitialButtonComp :title="'Aproveite nosso sistema interativo'" class="z-10 mt-14 p-4 md:hidden" />
-            <InitialButtonComp :title="'LOW'" class="hidden z-10 mb-14 p-4 md:flex" :hasArrowIcon="false" />
+            <InitialButtonComp :title="'Aproveite nosso sistema interativo'" class="z-10 mt-14 px-4 py-1.5 xl:hidden" />
+            <InitialButtonComp class="hidden z-10 mb-12 p-4 xl:flex xl:absolute xl:bottom-0 xl:mb-5" :hasArrowIcon="true" />
         </div>
-        <div class="col-start-1 row-start-3 flex items-end z-10 md:hidden">
+        <div class="col-start-1 row-start-3 flex items-end z-10 xl:hidden">
             <IFC_LogoComp :isGreen="false" :margin="'0px 0px 50px 30px'" />
         </div>
         <div class="absolute w-dvw h-dvh z-0 overlay"></div> 
@@ -32,7 +32,7 @@ import BoxButtonComp from '@/components/BoxButtonComp.vue'
     background-color: rgba(255, 255, 255, 0.23);
 }
 
-@media (max-width: 768px) {
+@media (max-width: 1280px) {
     .btn-access {
         background-color: transparent;
     }
--- src/components/TitleInitialPageComp.vue
+++ src/components/TitleInitialPageComp.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="w-auto h-auto flex flex-col gap-5 items-center justify-center md:gap-72 md:mt-10">
-        <h2 class="text-2xl font-normal font-poppins text-white text-center md:hidden">Instituto Federal Catarinense - Campus Araquari</h2>
-        <h2 class="hidden text-2xl font-normal font-poppins text-white text-center md:block">IFC - Campus Araquari</h2>
-        <h1 class="text-7xl font-semibold font-poppins text-white text-center leading-tight">Atlas Digital de Histologia</h1>
+    <div class="w-auto h-auto flex flex-col gap-2 items-center justify-center md:gap-52 xl:mb-24 md:mt-10">
+        <h2 class="text-2xl font-normal font-poppins text-zinc-200 text-center xl:hidden tracking-widest">Instituto Federal Catarinense - Campus Araquari</h2>
+        <h2 class="hidden text-3xl font-extralight font-poppins text-white text-center xl:block">IFC - Campus Araquari</h2>
+        <h1 class="text-7xl font-semibold font-poppins text-white text-center leading-tight xl:hidden">Atlas Digital de Histologia</h1>
+        <h1 class="hidden xl:block text-7xl font-semibold font-poppins text-white text-center leading-tight">Atlas de Histologia</h1>
     </div>
 </template>
