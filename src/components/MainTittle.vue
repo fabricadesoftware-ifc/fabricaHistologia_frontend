507373ac8257c37b12999e8a937c3dfd87f3b160
jaotarzan <joaovssouza59@gmail.com>
2024-08-08 14:53:25 -0300

    FEAT: creating the tittle component

---
+++ src/components/MainTittle.vue
@@ -0,0 +1,30 @@
+<script setup>
+const props = defineProps({
+    'color': {
+        type: String,
+        default: '#181A2A'
+    },
+    'weight': {
+        type: String,
+        default: 'bold'
+    },
+    'content': {
+        type: String,
+        default: 'Tittle'
+    },
+    'size': {
+        type: String,
+        default: '4xl'
+    },
+    'boxSize': {
+        type: String,
+        default: '1/2'
+    }
+})
+</script>
+
+<template>
+    <div :class="`w-${props.boxSize} flex justify-center flex-wrap flex-row`">
+        <span :class="`font-${props.weight} text-[${props.color}] font-poppins text-${props.size}`">{{ props.content }}</span>
+    </div>
+</template>
