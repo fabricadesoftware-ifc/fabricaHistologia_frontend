82758755b4e107a7caf2b09777c11f476b2cdc3d
Jaotarzan <joaovssouza59@gmail.com>
2024-08-14 15:00:13 -0300

    FEAT: Creating AboutCollaborators Component #36

--- src/components/aboutPage/AboutPage.vue
+++ src/components/aboutPage/AboutPage.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AboutHeader from './AboutHeader.vue';
-import AboutContent from './AboutContent.vue'
+import AboutContent from './AboutContent.vue';
+import AboutCollaborators from './AboutCollaborators.vue';
 </script>
 
 <template>
@@ -9,6 +10,10 @@ import AboutContent from './AboutContent.vue'
         <AboutContent title="Pesquisa" description="Atualmente, mudanças constantes e significativas, especialmente na área de tecnologia e informação, estão refletindo importantes transformações comportamentais e, consequentemente na forma de aprender e de consolidar o conhecimento. O ensino atual requer a inserção de diferentes recursos, principalmente os que apresentem um acesso rápido, fácil, dinâmico e que estimulem a interatividade. A PUCRS, como uma das melhores universidades do país, tem um papel importante na promoção da inovação dessa nova Atualmente, mudanças constantes e significativas, especialmente na área de tecnologia e informação, estão refletindo importantes transformações comportamentais e, consequentemente na forma de aprender e de consolidar o conhecimento. O ensino atual requer a inserção de diferentes recurso."/>
         <AboutContent title="Desenvolvimento" description="Atualmente, mudanças constantes e significativas, especialmente na área de tecnologia e informação, estão refletindo importantes transformações comportamentais e, consequentemente na forma de aprender e de consolidar o conhecimento. O ensino  atual requer a inserção de diferentes recurso, especialmente na área de tecnologia e informação, estão refletindo importantes transformações comportamentais e, consequentemente na forma de aprender e de consolidar o conhecimento. O ensino atual requer a inserção de diferentes recurso" />
         <AboutContent title="Equipe" description="Atualmente, mudanças constantes e significativas, especialmente na área de tecnologia e informação, estão refletindo importantes transformações comportamentais e, consequentemente na forma de aprender e de consolidar o conhecimento. O ensino  atual requer a inserção." />
+        <AboutCollaborators position="Professor Coordenador" :collaborators="['Jayrson Araújo de Oliveira']" />
+        <AboutCollaborators position="Professores participantes" :collaborators="['Gilson Herbert de Faria;', 'Mara Rubia Marques']" />
+        <AboutCollaborators position="Estagiário" :collaborators="['Miller Caldas Barradas']" />
+        <AboutCollaborators position="Prestadores de serviços" :collaborators="['Web-Master e Web-Designer:', 'Swami Luiz Monteiro Silva', 'Editoração de imagens:', 'Julia Cavasin Oliveira']" />
         
     </div>
 </template>
