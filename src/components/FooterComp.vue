3a052c8b7a443911205f1a0d2515e1c957e57f84
Laíssa Mendes <laissa.mendes2006@gmail.com>
2024-08-22 16:15:27 -0300

    Footer almost complete

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,31 @@
+<script setup></script>
+
+<template>
+    <footer>
+      <div>
+        <img src="@/assets/media/logoFabrica.png" alt="" class="logoFabrica">
+        <img src="@/assets/media/ifcLogoGreen.png" alt="">
+        </div>
+        <img src="@/assets/media/imgHelp.png" alt="">    
+        </footer>
+</template>
+
+<style scoped>
+footer>div{
+    display: flex;
+    align-items: baseline;
+}
+footer>div>img{
+    margin: 1rem;
+    width: 4rem;
+}
+footer{
+    display: flex;
+    align-items: baseline;
+    justify-content: space-between;
+    padding-left: 10rem;
+    padding-right: 10rem;
+    background-color: black;
+}
+
+</style>
--- src/views/tests/ComponentsView.vue
+++ src/views/tests/ComponentsView.vue
@@ -1,26 +1,21 @@
 <script setup>
 import { ref } from 'vue';
-import {
-  AddInfoGlobal,
-  BackButton,
-  BoxButton,
-  BtnDefault,
-  BtnStroke,
-  CardsContainer,
-  ContainerGlobal,
-  DefaultTitle,
-  InitialSearchButton,
-  MediumCardDefault,
-  NavbarHome,
-  SearchBar,
-  SearchButtonGlobal,
-  SearchResults,
-  SlideCard,
-  SmallCardDefault,
-  TitleGlobal,
-  TitleHome,
-  TopicsAdditionalInfo
-} from '@/components/index'
+
+import ContainerDefault from '@/components/ContainerDefault.vue';
+import AdditionalInfo from '@/components/AdditionalInfo.vue';
+import BackButton from '../../components/BackButton.vue';
+import DefaultTitle from '@/components/DefaultTitle.vue';
+import CardsContainer from '@/components/CardsContainer.vue';
+import SlideCard from '@/components/SlideCard.vue';
+import FooterComp from '@/components/FooterComp.vue'
+
+const objs = ref([
+    {name: 'Nome da lâmina', date: ['10', 'agosto', '2028'], tags: [{color: 'text-white', background: 'bg-purple-700', content: '2x'}, {color: 'text-white', background: 'bg-green-600', content: 'Boi'}, {color: 'text-black', background: 'bg-gray-400', content: 'tag'} ]},
+    {name: 'Nome da lâmina', date: ['10', 'agosto', '2028'], tags: [{color: 'text-white', background: 'bg-purple-700', content: '2x'}, {color: 'text-white', background: 'bg-green-600', content: 'Boi'}, {color: 'text-black', background: 'bg-gray-400', content: 'tag'} ]},
+    {name: 'Nome da lâmina', date: ['10', 'agosto', '2028'], tags: [{color: 'text-white', background: 'bg-purple-700', content: '2x'}, {color: 'text-white', background: 'bg-green-600', content: 'Boi'}, {color: 'text-black', background: 'bg-gray-400', content: 'tag'} ]},
+    {name: 'Nome da lâmina', date: ['10', 'agosto', '2028'], tags: [{color: 'text-white', background: 'bg-purple-700', content: '2x'}, {color: 'text-white', background: 'bg-green-600', content: 'Boi'}, {color: 'text-black', background: 'bg-gray-400', content: 'tag'} ]},
+    {name: 'Nome da lâmina', date: ['10', 'agosto', '2028'], tags: [{color: 'text-white', background: 'bg-purple-700', content: '2x'}, {color: 'text-white', background: 'bg-green-600', content: 'Boi'}, {color: 'text-black', background: 'bg-gray-400', content: 'tag'} ]},
+])
 
 const datas_AdditionalInfo = [
   {
@@ -69,16 +64,21 @@ const objs = ref([
         <CardsContainer />
         <ContainerGlobal />
         <DefaultTitle />
-        <InitialSearchButton />
-        <MediumCardDefault />
-        <NavbarHome />
-        <SearchBar />
-        <SearchButtonGlobal />
-        <SearchResults />
-        <SlideCard />
-        <SmallCardDefault />
-        <TitleGlobal />
-        <TitleHome />
-        <TopicsAdditionalInfo />
+        <hr>
+
+        <br>
+
+        <hr>
+        <p>CardsContainer with SlideCard</p>
+        <CardsContainer>
+          <SlideCard :details="objs" />
+        </CardsContainer>
+        <hr>
+        <br>
+        <hr>
+        <p>Footer</p>
+        <FooterComp />
+        <hr>
+
     </main>
 </template>
