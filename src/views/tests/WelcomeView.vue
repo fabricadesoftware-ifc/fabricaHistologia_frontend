e3a4dc6a0ffeae55a92d79c851e27f47824d8a34
Gabz047 <gabe1612mail@gmail.com>
2024-08-28 20:40:25 -0300

    FEAT: Fixed spacing of elements and removed useless images

--- src/views/tests/WelcomeView.vue
+++ src/views/tests/WelcomeView.vue
@@ -4,10 +4,10 @@ const navigationStore = useNavigationStore()
 import {CardsContainer, ContainerGlobal, DefaultTitle, MediumCardDefault} from '@/components/index';
 </script>
 <template>
-    <main class="w-dvw h-dvh bg-green-200">
-            <ContainerGlobal class="flex justify-center items-center flex-col gap-10">
+    <main class="w-dvw h-dvh">
+            <ContainerGlobal class="flex justify-center items-center flex-col gap-16">
             <DefaultTitle :color="'text-black'" :weight="'font-medium'" :content="'Bem-vindo ao Portal! Explore nossos recursos'" :size="'text-3xl'" :position="'justify-center'" />
-            <CardsContainer class="bg-gray-300 justify-between" >
+            <CardsContainer class="justify-between mb-16" >
                 <MediumCardDefault v-for="(item, index) in navigationStore.state.navPortal" :key="index" :icon="item.icon" :title="item.name" />
             </CardsContainer>
             </ContainerGlobal>
