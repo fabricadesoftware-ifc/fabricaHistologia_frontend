286fb0dd9d9c61f41aad32c91676c7a78b79bd32
Gabz047 <gabe1612mail@gmail.com>
2024-08-28 19:37:49 -0300

    FIX: Fixed welcome css

--- src/views/tests/WelcomeView.vue
+++ src/views/tests/WelcomeView.vue
@@ -5,11 +5,10 @@ import DefaultTitle from '@/components/DefaultTitle.vue';
 </script>
 <template>
     <main class="w-dvw h-dvh bg-green-200">
-            <ContainerDefault class="flex justify-center items-center flex-col">
-            <h1 class=" font-poppins text-xl font-medium"></h1>
-            <DefaultTitle :color="'text-black'" :weight="'font-medium'" :content="'Bem-vindo ao Portal! Explore nossos recursos'" :size="'text-xl'" :position="'justify-center'" />
-            <CardsContainer class="bg-gray-300  ">
-                <p>cards will be here soon.</p>
+            <ContainerDefault class="flex justify-center items-center flex-col gap-10">
+            <DefaultTitle :color="'text-black'" :weight="'font-medium'" :content="'Bem-vindo ao Portal! Explore nossos recursos'" :size="'text-3xl'" :position="'justify-center'" />
+            <CardsContainer class="bg-gray-300 justify-between" >
+                <div class=" min-h-64 min-w-72 bg-gray-700" v-for="(item, index) in 3" :key="index"></div>
             </CardsContainer>
             </ContainerDefault>
     </main>
