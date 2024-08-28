79c9d114349fcea6a9b89c0466173daf02eb07c3
Gabz047 <gabe1612mail@gmail.com>
2024-08-28 20:13:29 -0300

    FEAT: Rebased with dev and created navPortal array in navigation store

--- src/views/tests/WelcomeView.vue
+++ src/views/tests/WelcomeView.vue
@@ -1,15 +1,15 @@
 <script setup>
-import CardsContainer from '@/components/CardsContainer.vue';
-import ContainerDefault from '@/components/ContainerDefault.vue';
-import DefaultTitle from '@/components/DefaultTitle.vue';
+import { useNavigationStore } from '@/stores/blog/navigation';
+const navigationStore = useNavigationStore()
+import {CardsContainer, ContainerGlobal, DefaultTitle, MediumCardDefault} from '@/components/index';
 </script>
 <template>
     <main class="w-dvw h-dvh bg-green-200">
-            <ContainerDefault class="flex justify-center items-center flex-col gap-10">
+            <ContainerGlobal class="flex justify-center items-center flex-col gap-10">
             <DefaultTitle :color="'text-black'" :weight="'font-medium'" :content="'Bem-vindo ao Portal! Explore nossos recursos'" :size="'text-3xl'" :position="'justify-center'" />
             <CardsContainer class="bg-gray-300 justify-between" >
-                <div class=" min-h-64 min-w-72 bg-gray-700" v-for="(item, index) in 3" :key="index"></div>
+                <MediumCardDefault v-for="(item, index) in navigationStore.state.navPortal" :key="index" :icon="item.icon" :title="item.name" />
             </CardsContainer>
-            </ContainerDefault>
+            </ContainerGlobal>
     </main>
 </template>
