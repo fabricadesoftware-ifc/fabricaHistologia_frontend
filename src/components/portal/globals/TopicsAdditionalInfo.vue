a76d488d6ebad1a5da996546d31c6ea44ff1fd3b
mateus-lopes <mateusalbano22@gmail.com>
2024-09-01 03:12:14 -0300

    fix: page corrections #25

--- src/components/portal/globals/TopicsAdditionalInfo.vue
+++ src/components/portal/globals/TopicsAdditionalInfo.vue
@@ -12,7 +12,7 @@ const props = defineProps({
     <h4 class="text-xl font-medium">{{ item.title }}</h4> 
     <ul>
       <li v-for="(ref, index) in item.refs" :key="index" class="mt-3">
-        <a href="#" class="text-stone-700 font-normal font-poppins">{{ ref.link }}</a>
+        <a href="#" class="text-stone-700 font-normal font-poppins hover:underline">{{ ref.link }}</a>
         <ul class="list-disc ml-5">
           <li class="text-stone-700 font-normal font-poppins">{{ ref.desc }}</li>
         </ul>   
