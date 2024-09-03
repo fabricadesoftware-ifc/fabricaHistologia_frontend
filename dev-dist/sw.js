e06bd5efe424466f77e2872966a4faf32657b266
Gabz047 <gabe1612mail@gmail.com>
2024-09-03 13:20:59 -0300

    FEAT: Created Quiz Home Responsivity #29

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.g6sah4dlb9"
+    "revision": "0.5onho0itul8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/views/portal/QuizHomeView.vue
+++ src/views/portal/QuizHomeView.vue
@@ -9,17 +9,17 @@ const buttons = [
 </script>
 <template>
 <main class=" w-full h-dvh flex flex-col justify-between">
-    <ContainerGlobal class="gap-7 h-full flex flex-col items-center justify-center">
+    <ContainerGlobal class="gap-7 h-full flex flex-col items-center justify-center sm:mt-8">
 
         <TitleGlobal class="text-center" :color="'text-black'" :weight="'font-medium'" :content="'Quiz de Conhecimentos Gerais'" :size="'text-3xl'" :position="'justify-center'" />
 
         <TitleGlobal class="w-10/12 text-center" :color="'text-gray-500'" :weight="'font-medium'" :content="'Este é um quiz para testar seu conhecimento em Histologia. Ele reúne perguntas aleatórias cadastradas pelos colaboradores e varia a cada vez. Um ótimo método para estudar!'" :size="'text-lg'" :position="'justify-center'" />
 
-        <CardsContainer class="w-auto gap-10">
+        <CardsContainer class="w-auto gap-10 md:gap-5 sm:flex-wrap md:justify-center">
             <BtnDefault v-for="item, index in buttons" :key="index"  :text="item.text" :background="item.background"/>
         </CardsContainer>
     </ContainerGlobal>
-    <FooterComp />
+    <FooterComp class="sm:mt-14"/>
 </main>
 
 </template>
