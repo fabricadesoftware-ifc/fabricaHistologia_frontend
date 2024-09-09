47af33bd0ba583d347a6c322ff344f6c5c1e0423
jaotarzan <joaovssouza59@gmail.com>
2024-09-09 16:05:06 -0300

    Feat: Creating the responsive design

--- src/components/portal/search/SearchBar.vue
+++ src/components/portal/search/SearchBar.vue
@@ -9,12 +9,12 @@ const history = ref('Pulmão de Cachorro')
     <ContainerGlobal>
         <div class="w-3/4 mx-auto">
             <div class="bg-[#F1F1F1] w-full rounded-lg h-12 flex">
-                <input type="text" placeholder="pesquisar..." class="bg-transparent my-auto ml-10 outline-none w-5/6">
+                <input type="text" placeholder="pesquisar..." class="bg-transparent my-auto ml-10 sm:ml-5 outline-none w-5/6">
                 <i class="mdi mdi-magnify text-black text-4xl m-auto" />
             </div>
             <div class="w-full flex justify-end mt-2">
-                <span class="text-[#787878] text-end mr-2 text-sm">Ultima Pesquisa:</span>
-                <RouterLink to="#" class="text-[#787878] underline decoration-1 text-sm sm:text-sm">{{ history }}</RouterLink>
+                <span class="text-[#787878] text-end mr-2 text-sm sm:text-xs">Ultima Pesquisa:</span>
+                <RouterLink to="#" class="text-[#787878] underline decoration-1 text-sm sm:text-xs">{{ history }}</RouterLink>
             </div>
         </div>
     </ContainerGlobal>
