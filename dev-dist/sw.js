65ccea44bcb88432555ce75cdc8b36b98f457a2d
Gabz047 <gabe1612mail@gmail.com>
2024-09-12 12:34:30 -0300

    REFACTOR: Made footer responsive and added an smaller ifc logo to mobile screens #44

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hesjt3kb0g8"
+    "revision": "0.nicq4iq65p"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
