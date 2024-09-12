c7c5064c7a434451cb82c059d9f4fbb8c6f56bf1
Gabz047 <gabe1612mail@gmail.com>
2024-09-12 17:40:17 -0300

    FEAT: Added efects to home button #44

--- src/components/institutional/home/TitleHome.vue
+++ src/components/institutional/home/TitleHome.vue
@@ -8,13 +8,29 @@ import { BoxButton } from '@/components/index';
         <h1 class="text-7xl font-semibold font-poppins text-white text-center leading-tight xl:hidden">Atlas Digital de Histologia</h1>
         <h1 class="hidden xl:block text-7xl font-semibold font-poppins text-white text-center leading-tight">Atlas de Histologia</h1>
 
-        <a href="#" class="bg-gray-400 rounded-full w-16 h-16 mt-8 hidden md:block">
+        <router-link to="/portal" class="bg-gray-400 rounded-full w-16 h-16 mt-8 ease-in-out duration-100 hover:brightness-90  hidden md:block animation-rotate">
             <img src="@/assets/images/icons/arrowRight.svg" class="w-full h-full p-4" />
-        </a>
+        </router-link>
         <BoxButton title="Aproveite nosso sistema interativo" link="/portal" class="z-10 mt-14 px-8 py-2 md:hidden" hasArrowIcon />
     </div>
 </template>
 <style scoped>
+
+.animation-rotate {
+    transition: 0.3s;
+    & img {
+        transition: 0.3s;
+    }
+
+    &:hover img {
+        width: 200px;
+        transform: scale(1.2);
+    }
+
+    &:active {
+        transform: scale(1.1);
+    }
+}
 
 @media (max-width: 768px) {
     .btn-access {
