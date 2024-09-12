68bc360480d86fa4fe06d7008cf107d2dc40659f
Gabz047 <gabe1612mail@gmail.com>
2024-09-12 12:34:30 -0300

    REFACTOR: Made footer responsive and added an smaller ifc logo to mobile screens #44

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,15 +1,17 @@
 <script setup>
+import { ref } from 'vue';
 import { ContainerGlobal } from '..';
-
+const getWidthScreenSize = ref(window.innerWidth)
 </script>
 
 <template>
     <footer class="bg-[#F9F9F9] border">
         <ContainerGlobal>
-            <section class="flex justify-between items-center">
-                <div>
+            <section class="flex justify-between w-full items-center">
+                <div class="sm:flex sm:items-center sm:justify-around w-2/3 sm:gap-1">
                     <img src="@/assets/images/icons/logoFabrica.png" alt="">
-                    <img src="@/assets/images/icons/ifcLogoGreen.png" class="logoIf" alt="">
+                    <img v-if="getWidthScreenSize > 440" src="@/assets/images/icons/ifcLogoGreen.png" class="logoIf" alt="">
+                    <img v-else src="@/assets/images/icons/logoIFCmobile.png" class="ifLogoMobile">
                 </div>
                 <img src="@/assets/images/icons/imgHelp.png" class="w-8 h-8" alt="">    
             </section>
@@ -28,6 +30,10 @@ section>div>img{
 }
 .logoIf{
     width: 170px;
+}
+
+.ifLogoMobile {
+    width: 60px;
 }
 /* tailwind, usado os components (rebase) */
 </style>
