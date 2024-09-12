2476cd634d2198f69231b50e827493f087401707
Gabz047 <gabe1612mail@gmail.com>
2024-09-12 17:40:17 -0300

    FEAT: Added efects to home button #44

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7469l9v9dgg"
+    "revision": "0.8ucg5qtfk2"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
