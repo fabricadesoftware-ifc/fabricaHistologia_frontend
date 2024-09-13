2192c3170bf94162dd0ea1b381a3ea0a96e33a0e
jaotarzan <joaovssouza59@gmail.com>
2024-09-13 16:52:58 -0300

    FEAT: Adding the static images to the welcome page

--- src/stores/blog/navigation.js
+++ src/stores/blog/navigation.js
@@ -7,9 +7,9 @@ export const useNavigationStore = defineStore("navigation", () => {
     history: [],
     currentPage: null,
     navPortal: [
-      {name: 'Atlas Digital', icon: 'https://cdn-icons-png.freepik.com/256/3407/3407022.png?semt=ais_hybrid', link: '/portal/system'},
-      {name: 'Conteudos', icon: 'https://cdn-icons-png.freepik.com/256/3407/3407022.png?semt=ais_hybrid', link: '/portal/content'},
-      {name: 'Quiz', icon: 'https://cdn-icons-png.freepik.com/256/3407/3407022.png?semt=ais_hybrid', link: '/portal/quiz'},
+      {name: 'Atlas Digital', icon: 'https://res.cloudinary.com/dzdrwmug3/image/upload/v1726256669/vet/images/04397255-55b1-42cf-8d34-a6ba7f8b5a44_oclmlh.png', link: '/portal/system'},
+      {name: 'Conteudos', icon: 'https://res.cloudinary.com/dzdrwmug3/image/upload/v1726256653/vet/images/0d41809a-dfe4-42c8-b7d6-647545805234_sks6kl.png', link: '/portal/content'},
+      {name: 'Quiz', icon: 'https://res.cloudinary.com/dzdrwmug3/image/upload/v1726256611/vet/images/092796fb-7e18-48c3-a0bb-882d35d5298b_fubud7.png', link: '/portal/quiz'},
     ]
   });
 
