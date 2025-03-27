68d56402e91dde144245c9fcf7c5013a7427318d
Gabz047 <gabe1612mail@gmail.com>
2025-03-27 19:03:28 -0300

    FIX: Fixed project responsivity

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,16 @@ export default {
     fontFamily: {
       poppins: ["Poppins", "sans-serif"]  
     },
-    screens: {
-      '2.5xl': { 'max': '1700px'},  
-      '2xl': { 'max': '1536px'},
-      'xl': { 'max': '1280px'},
-      'lg': { 'max': '1024px'},
-      'md2': {'max': '900px'},
-      'md': { 'max': '768px'},
-      'sm': { 'max': '640px'},
-      'xsm': {'max': '450px'},
-    },
+    // screens: {
+    //   '2.5xl': { 'max': '1700px'},  
+    //   '2xl': { 'max': '1536px'},
+    //   'xl': { 'max': '1280px'},
+    //   'lg': { 'max': '1024px'},
+    //   'md2': {'max': '900px'},
+    //   'md': { 'max': '768px'},
+    //   'sm': { 'max': '640px'},
+    //   'xsm': {'max': '450px'},
+    // },
     extend: {
       minWidth: {
         '76': '300px'
