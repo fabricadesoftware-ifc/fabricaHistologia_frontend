2181f27f67105eabce66a05ce9924080b06f2e8c
Gabz047 <gabe1612mail@gmail.com>
2025-05-29 18:55:56 -0300

    FEAT: Added button to select post type

--- src/views/portal/SlidesView.vue
+++ src/views/portal/SlidesView.vue
@@ -4,7 +4,6 @@ import { useRoute, useRouter } from 'vue-router';
 import { SpecieFilterComponent } from '@/components/index';
 import { useOrganStore, usePostStore, useNavigationStore, useSystemStore, useQuizStore } from '@/stores';
 import { resetAll } from '@/utils/quiz';
-const navigationStore = useNavigationStore()
 import {
   ContainerGlobal,
   CardsContainer,
@@ -13,7 +12,6 @@ import {
   BtnDefault,
   Footer,
   HeaderPortal,
-  MessageGlobal
 } from '@/components/index'
 const systemStore = useSystemStore()
 const organStore = useOrganStore()
@@ -25,22 +23,20 @@ const organ_id = router.params.id
 const system_id = systemStore.selectedSystem.id
 
 const buttons = ref([
-  {text: 'Patologia', selected: true, post: 2},
-  {text: 'Histologia', selected: false, post: 1}
+  {text: 'Histologia', selected: true, post: 1},
+  {text: 'Patologia', selected: false, post: 2}
 ])
 
-// const selectPostType = (item) => {
-//   for (let state of buttons.value) {
-//     state.selected = !state.selected
-//   }
-//   postStore.getPostsByOrganAndType(organ_id, item.post)
-// }
-
+const selectPostType = (item) => {
+  for (let state of buttons.value) {
+    state.selected = !state.selected
+  }
+  postStore.getPostsByOrganAndType(organ_id, item.post, '')
+}
 
 onMounted(async()=>{
   await organStore.getOrgansById(organ_id)
   await postStore.getPostsByOrganAndType(organ_id, 1, '')
-  console.log(system_id)
 })
 
 const push = async(id) => {
@@ -52,17 +48,20 @@ const push = async(id) => {
 </script>
 <template>
   <main class=" min-h-screen-minus-80 relative">
-    <HeaderPortal class=" " :title="organStore.selectedOrgan.name" />
+    <HeaderPortal :title="organStore.selectedOrgan.name" />
    
     <SpecieFilterComponent />
 
-    <div class=" flex justify-center gap-10">
+     <div class=" flex justify-center gap-10">
+      <p @click="selectPostType(button)" :class="button.selected ? 'text-[#267A7A] text-[18px] font-semibold selectedType' : ' hover:opacity-75' + ' duration-200 ease-in-out cursor-pointer'" v-for="(button, index) in buttons" :key="index" >{{ button.text }}</p>
+      
     </div>
+   
     <div class="w-full h-96 flex justify-center items-center flex-col" v-if="postStore.postByOrganAndType.length  == 0">
     <h1 class="text-3xl md:text-5xl text-center break-all">Lâminas não encontradas</h1>
-    <p class="text-xl break-all">Não há nenhuma lâmina registrada no portal</p>
     </div>
     <section v-else>
+      
     <CardsContainer :gap="'gap-10'" :justify="'justify-start'" class="mb-8 mt-8 sm:mt-16">
       <SlideCards :data="postStore.postByOrganAndType" />
     
@@ -76,3 +75,22 @@ const push = async(id) => {
   </main>
   <Footer class="relative bottom-0" />
 </template>
+
+<style scooped>
+.selectedType::before {
+content: '•   ';
+animation: appear 0.3s linear;
+}
+
+@keyframes appear {
+  from {
+    content: '•   ';
+    font-size: 10px;
+  }
+  to {
+    content: '•   ';
+    font-size: 18px;
+    transform: scale(1.1);
+  }
+}
+</style>
