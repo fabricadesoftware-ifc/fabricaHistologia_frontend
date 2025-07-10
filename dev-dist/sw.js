9cc96939c01620879bf8c3a40ed9eb8cfb67282d
Gabz047 <gabe1612mail@gmail.com>
2025-07-10 10:31:55 -0300

    FIX: Fixing pwa problem

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,11 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
+<<<<<<< HEAD
     "revision": "0.h56v0425i2"
+=======
+    "revision": "0.8h88kr6sbk8"
+>>>>>>> e4395fd (FIX: Fixing pwa problem)
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
