fb3749f538c42543388a201b799411ca4eb7e97c
HenriqueChepli <henriquechepli@gmail.com>
2025-07-26 18:27:21 -0300

    feat: adicionado imagens no componente de InputImageAdmin #116

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bchj0jbeikg"
+    "revision": "0.0f6s2t6jo0g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
