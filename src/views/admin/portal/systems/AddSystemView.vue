f62c05f14247ef8ebc7b18ee9c2d8e2a9731627b
Jaotarzan <joaovssouza59@gmail.com>
2025-08-18 18:10:24 -0300

    FEAT: Adicionar gerenciamento de sistemas com rotas e componentes

--- src/views/admin/portal/systems/AddSystemView.vue
+++ src/views/admin/portal/systems/AddSystemView.vue
@@ -1,1 +1,76 @@
-<template></template>
+<script setup>
+import { onMounted, reactive, ref } from 'vue'
+import { useAuthStore, useUploadStore, useSystemStore } from '@/stores'
+import {
+  NavLateralAdmin,
+  ButtonActionAdmin,
+  InputDateAdmin,
+  InputImageAdmin,
+  InputSelectAdmin,
+  InputStringAdmin,
+  AdminGlobalContainer,
+  BtnDefault,
+} from '@/components/index'
+import router from '@/router'
+
+const authStore = useAuthStore()
+const uploadStore = useUploadStore()
+const systemsStore = useSystemStore()
+
+const newSystem = reactive({
+  name: '',
+  description: '',
+  image: null
+})
+
+const newImage = reactive({
+  file: null,
+  description: '',
+})
+
+onMounted(async ()=> {
+  console.log('Dados iniciais do sistema:', newSystem)
+})
+
+const send = async () => {
+  try {
+    if (newImage.file) {
+    const imageUiqueDescriptionId = crypto.randomUUID()
+    newImage.description = `${newSystem.name} - ${imageUiqueDescriptionId}`
+    newSystem.autor_user = authStore.userInfo.id
+
+    const result = await uploadStore.createUpload(`images`, newImage)
+    console.log('imagem criada', result)
+
+    newSystem.image = result.attachment_key
+    console.log(newSystem)
+    }
+    await systemsStore.createSystem(newSystem)
+
+    router.push('/admin/systems')
+  } catch (err) {
+    console.error('Erro ao fazer upload da imagem:', err)
+  }
+}
+</script>
+
+<template>  
+    
+    <AdminGlobalContainer>
+    <div class="w-[90%] mx-auto space-y-6">
+
+      <form class="grid grid-cols-1 md:grid-cols-2 gap-10 w-full" @submit.prevent="send">
+        <InputStringAdmin label="Nome" :modelValue="newSystem.name" @action="newSystem.name = $event"/>
+
+        <InputStringAdmin label="Descrição" :modelValue="newSystem.description" @action="newSystem.description = $event"/>
+
+
+        <div class="md:col-span-2 mb-10">
+          <InputImageAdmin label="Imagem" :modelValue="newImage.file" @action="newImage.file = $event"/>
+        </div>
+
+        <BtnDefault class="mb-10" text="Cadastrar" background="bg-[#29AC96]" :hasLink="false" />
+      </form>
+    </div>
+  </AdminGlobalContainer>
+</template>
--- src/views/admin/portal/systems/SystemView.vue
+++ src/views/admin/portal/systems/SystemView.vue
@@ -1,1 +1,52 @@
-<template></template>
+<script setup>
+import { onMounted } from 'vue'
+import { useSystemStore } from '@/stores'
+import {
+  TableFilterContainer,
+  TableFilterCard,
+  ButtonActionAdmin,
+  AdminGlobalContainer
+} from '@/components/index'
+
+import { useAdmin } from '@/stores/admin/filter_admin'
+const systemStore = useSystemStore()
+const { generalFilterData, changeActive } = useAdmin()
+
+onMounted(async () => {
+  await systemStore.getSystems()
+  console.log(systemStore.systems)
+})  
+
+</script>
+
+<template>
+ 
+  <AdminGlobalContainer >
+
+    <div class="flex gap-5 h-56 items-center justify-between ">
+     <ButtonActionAdmin />
+     <div class=" bg-blue-400 h-[80%] w-[40%] mr-[5%] mt-10 mb-10"></div>
+    </div>
+    
+    
+    <section>
+      <div class="flex flex-col w-[90%] mx-auto">
+        <p class="text-xl font-medium mb-10">Cadastros Gerais</p>
+        <TableFilterContainer :amount="generalFilterData.length">
+          <TableFilterCard
+            @change="changeActive(index, generalFilterData)"
+            :active="i.active"
+            :filter="i"
+            v-for="(i, index) in generalFilterData"
+            :key="index"
+          />
+        </TableFilterContainer>
+      </div>
+
+      <div class="p-16" v-for="(system, index) in systemStore.systems" :key="index">
+        <p> {{system.id}} - {{ system.name }}</p>
+        <img class=" w-24 h-24 object-cover" :src="system?.image?.url">
+      </div>
+    </section>
+  </AdminGlobalContainer>
+</template>
