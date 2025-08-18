e0c8a2416b115ac0e81307ea6b4eaaec616cf256
Gabz047 <gabe1612mail@gmail.com>
2025-08-18 16:08:58 -0300

    FEAT: Create post

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.nk590gc1mn8"
+    "revision": "0.02spdt52um8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
